8bca98758a138a06b5ee968a39daeb67cb895f26
Filip Engström <filip@Filips-MacBook-Pro.local>
2016-09-20 22:27:14 +0200

    Initial commit

---
+++ src/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+var Input = React.createClass({
+	render: function() {
+		return (
+			<div className="Input">
+				<input 
+					id={this.props.name}
+					autoComplete="false"
+					required
+					type={this.props.type}
+					placeholder={this.props.placeholder}
+				/>	
+				<label htmlFor={this.props.name}></label>
+			</div>
+		);
+	}
+});
+
+export default Input;
---
+++ src/Modal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Input from './Input';
+
+var Modal = React.createClass({
+	render: function() {
+		return (
+			<div className="Modal">
+				<form 
+					onSubmit={this.props.onSubmit}
+					className="ModalForm">
+					<Input
+						id="name"
+						type="text"
+						placeholder="Jack-Edward Oliver" />
+					<Input
+						id="username"
+						type="email"
+						placeholder="mrjackolai@gmail.com" />
+					<Input
+						id="password"
+						type="password"
+						placeholder="password" />
+					<button>
+						Log in <i className="fa fa-fw fa-chevron-right"></i>
+					</button>
+				</form>
+			</div>
+		);
+	}
+});
+
+export default Modal;
